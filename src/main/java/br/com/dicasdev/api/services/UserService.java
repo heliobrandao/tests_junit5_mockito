4e7499d359ebe298077244738f711c8c08392e3c
Helio Serrano Brandao Junior <c066588@corp.caixa.gov.br>
2024-07-11 18:44:38 -0300

    create endpoint for user

--- src/main/java/br/com/dicasdev/api/services/UserService.java
+++ src/main/java/br/com/dicasdev/api/services/UserService.java
@@ -3,10 +3,11 @@ package br.com.dicasdev.api.services;
 import java.util.List;
 
 import br.com.dicasdev.api.domain.User;
+import br.com.dicasdev.api.domain.dto.UserDTO;
 
 public interface UserService {
 
     User findById(Integer id);
-
     List<User> findAll();
+    User create(UserDTO obj);
 }
--- src/main/java/br/com/dicasdev/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/dicasdev/api/services/impl/UserServiceImpl.java
@@ -3,10 +3,12 @@ package br.com.dicasdev.api.services.impl;
 import java.util.List;
 import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.dicasdev.api.domain.User;
+import br.com.dicasdev.api.domain.dto.UserDTO;
 import br.com.dicasdev.api.services.UserService;
 import br.com.dicasdev.api.services.exceptions.ObjectNotFoundException;
 import br.com.dicasdev.api.repositories.UserRepository;
@@ -17,6 +19,9 @@ public class UserServiceImpl implements UserService{
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
     public User findById(Integer id){
         Optional<User> obj = repository.findById(id);
@@ -25,6 +30,11 @@ public class UserServiceImpl implements UserService{
 
     public List<User> findAll(){
         return repository.findAll();
+    }
+
+    @Override
+    public User create(UserDTO obj) {
+        return repository.save(mapper.map(obj, User.class));
     }
 
 }
