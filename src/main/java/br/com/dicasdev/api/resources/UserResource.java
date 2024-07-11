297e8180fc09c1fcb8d6ad1e17568dee19dab2d1
Helio Serrano Brandao Junior <c066588@corp.caixa.gov.br>
2024-07-11 17:58:01 -0300

    findAll endpoint for user

--- src/main/java/br/com/dicasdev/api/resources/UserResource.java
+++ src/main/java/br/com/dicasdev/api/resources/UserResource.java
@@ -1,5 +1,8 @@
 package br.com.dicasdev.api.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -26,6 +29,15 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
         // return ResponseEntity.ok().body(new User(1, "helio", "email", "123"));
         return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
+    }
 
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll(){
+        return ResponseEntity.ok()
+            .body(service.findAll()
+                    .stream()
+                        .map(x -> mapper.map(x, UserDTO.class))
+                        .collect(Collectors.toList())
+            );
     }
 }
--- src/main/java/br/com/dicasdev/api/services/UserService.java
+++ src/main/java/br/com/dicasdev/api/services/UserService.java
@@ -1,8 +1,12 @@
 package br.com.dicasdev.api.services;
 
+import java.util.List;
+
 import br.com.dicasdev.api.domain.User;
 
 public interface UserService {
 
     User findById(Integer id);
+
+    List<User> findAll();
 }
